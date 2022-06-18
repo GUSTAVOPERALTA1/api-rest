0459b0f875d28e1df0a93b48825592afcfca35f6
GUSTAVOPERALTA1 <1719110275@utectulancingo.edu.mx>
2022-06-18 15:33:53 +0000

    nuevo contenedor

--- apitest/code/main.py
+++ apitest/code/main.py
@@ -17,22 +17,31 @@ app = FastAPI()
 @app.get("/", response_model=Respuesta)
 async def index():
     return{"message": "API-REST"}
+
+@app.get("/clientes/{id_cliente}")
+async def get_clientesid(id_cliente):
+    with sqlite3.connect('sql/clientes.sqlite') as connection:
+        connection.row_factory = sqlite3.Row
+        cursor=connection.cursor()
+        cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id_cliente)))
+        response=cursor.fetchall()
+        return response   
     
-#Metodo get con limit y offset    
+#Metodo GET con limit y offset    
 @app.get("/clientes/", response_model=List[Cliente])
-async def get_clientes(offset:int=0,limit:int=5):
+async def get_clientes(offset:int=0,limit:int=10):
     with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM clientes LIMIT ? OFFSET ?",(limit,offset))
         response = cursor.fetchall()
         return response
-
-@app.get("/clientes/{id_cliente}")
-async def clientes(id_cliente):
+#Metodo POST
+@app.post("/clientes/")
+async def post_clientes(cliente=Cliente):
     with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor=connection.cursor()
-        cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id_cliente)))
+        cursor.execute("INSERT INTO clientes(id_cliente,nombre,email)VALUES(?,?,?)",(cliente.id_cliente,cliente.nombre,cliente.email))
         response=cursor.fetchall()
-        return response        
+        return {"mensaje":"Cliente agregado"}
