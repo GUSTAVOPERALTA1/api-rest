75b08dd4c9e78e4f504f9444469be2650894286c
GUSTAVOPERALTA1 <1719110275@utectulancingo.edu.mx>
2022-06-21 04:34:31 +0000

    query funcionando

--- apitest/test/test_main.py
+++ apitest/test/test_main.py
@@ -8,3 +8,17 @@ def test_index():
     data = { "message":"API-REST" }
     assert response.status_code == 200
     assert response.json() == data
+
+def test_list():
+    response = clientes.get('/clientes/1')
+    data = [{"id_cliente":1,"nombre":"Saul","email":"saul@gmail.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_offset():
+    response = clientes.get('/clientes/?offeset=1&limit=3')
+    data = [{"id_cliente":1,"nombre":"Saul","email":"saul@gmail.com"},
+	{"id_cliente":2,"nombre":"Maria","email":"maria@gmail.com"},
+	{"id_cliente":3,"nombre":"Fatima","email":"fatima@gmail.com"}]
+    assert response.status_code == 200
+    assert response.json()==data
