65570b4b088631928c3c10cf150c94fcc6f428e0
GUSTAVOPERALTA1 <1719110275@utectulancingo.edu.mx>
2022-06-28 04:51:51 +0000

    api security

---
+++ apitest/code/main_old.py
@@ -0,0 +1,68 @@
+
+from fastapi import FastAPI
+import sqlite3
+from typing import List
+from pydantic import BaseModel
+
+class Respuesta(BaseModel):
+    message: str
+
+class Cliente(BaseModel):
+    id_cliente: int
+    nombre: str
+    email: str
+
+app = FastAPI()
+
+
+@app.get("/", response_model=Respuesta)
+async def index():
+    return{"message": "API-REST"}
+
+@app.get("/clientes/{id_cliente}")
+async def get_clientesid(id_cliente):
+    with sqlite3.connect('sql/clientes.sqlite') as connection:
+        cursor=connection.cursor()
+        cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id_cliente)))
+        response=cursor.fetchall()
+        return response   
+    
+#Metodo GET con limit y offset    
+@app.get("/clientes/", response_model=List[Cliente])
+async def get_clientes(offset:int=0,limit:int=11):
+    with sqlite3.connect('sql/clientes.sqlite') as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM clientes LIMIT ? OFFSET ?",(limit,offset))
+        response = cursor.fetchall()
+        return response
+    
+#Metodo POST
+@app.post('/insertar/{nombre}/{email}')
+async def post_clientes(nombre:str,email:str):
+	with sqlite3.connect('sql/clientes.sqlite') as connection:
+		connection.row_factory = sqlite3.Row
+		cursor = connection.cursor()
+		cursor.execute('INSERT INTO clientes (nombre,email) VALUES (?,?)',(nombre,email))
+		cursor.fetchall()
+		return {"mensaje":"Cliente agregado"}
+
+#Metodo PUT
+@app.put('/actulizar/{id}/{nombre}/{email}')
+async def put_clientes(id:str, nombre:str, email:str):
+	with sqlite3.connect('sql/clientes.sqlite') as connection:
+		connection.row_factory = sqlite3.Row
+		cursor = connection.cursor()
+		cursor.execute('UPDATE clientes SET nombre = ?, email = ? WHERE id_cliente = ?',(nombre,email,id))
+		cursor.fetchall()
+		return {"mensaje":"Cliente Actualizado"}
+
+#Metodo DELETE
+@app.delete('/eliminar/{id}')
+async def delete_clientes(id:int):
+	with sqlite3.connect('sql/clientes.sqlite') as connection:
+		connection.row_factory = sqlite3.Row
+		cursor = connection.cursor()
+		cursor.execute('DELETE FROM clientes WHERE id_cliente= {}'.format(int(id)))
+		cursor.fetchall()
+		return {"mensaje":"Cliente borrado"}
--- apitest/code/sql/clientes.sql
+++ apitest/code/sql/clientes.sql
@@ -15,3 +15,14 @@ INSERT INTO clientes(nombre,email) VALUES("Lupita","lupita@email.com");
 INSERT INTO clientes(nombre,email) VALUES("Carmen","carmen@email.com");
 .headers ON 
 SELECT * FROM clientes;
+
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios(
+    username TEXT,
+    password varchar(32),
+    level INTEGER
+);
+CREATE UNIQUE INDEX index_usuario ON usuarios(username);
+INSERT INTO usuarios(username, password, level) VALUES('admin','21232f297a57a5a743894a0e4a801fc3',0);
+INSERT INTO usuarios(username, password, level) VALUES('user','ee11cbb19052e40b07aac0ca060c23ee',1);
+SELECT * FROM usuarios;
